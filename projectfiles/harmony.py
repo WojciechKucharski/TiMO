98a044255f36fe8af94b48d880a8ab9a413ef5b8
Wojciech Kucharski <wojtekkucharski105@gmail.com>
2021-03-13 23:21:59 +0100

    FC

--- projectfiles/harmony.py
+++ projectfiles/harmony.py
@@ -2,10 +2,16 @@ import random as r
 from function1 import f
 
 class HM:
-    def __init__(self, HMSize = 10, varDim = 2):
+    def __init__(self, HMSize = 10, varDim = 2, fun = None):
+        self.start = 10
         self.HM = self.HMCreate(HMSize, varDim)
         self.eval = None
-        self.fun = None
+        self.fun = fun
+        self.HMCR = 0.95
+        self.PAR = 0.5
+        self.bw = 0.1
+        self.it = 0
+
 
     def HMCreate(self, HMSize = 10, varDim = 2):
         try:
@@ -21,10 +27,25 @@ class HM:
         for i in range(HMSize):
             new = []
             for j in range(varDim):
-                new.append(r.random() * 200 - 100)
-            new.append(None)
+                new.append(r.random() * 2 * self.start - self.start)
             HM.append(new)
         return HM
+
+    def addFun(self, fun):
+        try:
+            fun = str(fun)
+            self.fun = fun
+        except Exception as e:
+            print("Error, function not added.")
+            print(e)
+
+    def evaluate(self):
+        if self.isfun == False:
+            print("No function")
+            return -1
+        self.eval = []
+        for x in self.HM:
+            self.eval.append(f(self.fun, x))
 
     @property
     def isfun(self):
@@ -39,4 +60,49 @@ class HM:
     @property
     def varDim(self):
         return len(self.HM[0])
+
+    @property
+    def worst(self):
+        self.evaluate()
+        val = max(self.eval)
+        id = self.eval.index(val)
+        return [val, id, self.HM[id]]
+
+    @property
+    def best(self):
+        self.evaluate()
+        val = min(self.eval)
+        id = self.eval.index(val)
+        return [val, id, self.HM[id]]
+
+    def iteration(self):
+        x = []
+        for i in range(self.varDim):
+            if r.random() < self.HMCR:
+                x.append(r.choice(self.HM)[i])
+                if r.random() < self.PAR:
+                    x[-1]+=r.random()*self.bw
+            else:
+                x.append(r.random() * 2 * self.start - self.start)
+        if f(self.fun, x) < self.worst[0]:
+            self.HM[self.worst[1]] = x
+        self.it += 1
+
+    def calc(self, TolX, maxIter):
+        i = 0
+        while True:
+            i+=1
+            self.iteration()
+            a = self.worst[2]
+            b = self.best[2]
+            if ((a[0]-b[0])**2 + (a[1]-b[1])**2)**(0.5) < TolX:
+                print("TolX satisfied")
+                print("Harmony Search Stop: Iterations:{}".format(i))
+                print("Best solution: " + str(self.best[2]))
+                break
+            if i > maxIter:
+                print("Iteration overflow")
+                print("Harmony Search Stop: Iterations:{}".format(i))
+                print("Best solution: " + str(self.best[2]))
+                break
 
--- projectfiles/main.py
+++ projectfiles/main.py
@@ -1,2 +1,33 @@
 from function1 import f
 from harmony import HM
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+HMSize = 10
+n = 2
+#fun = "4*x1^2-2.1*x1^4+0.33*x1^6+x1*x2-4*x2^2+4*x2^4"
+fun = "x1^2+x2^2"
+
+a = HM(HMSize, n, fun)
+
+a.calc(0.00001, 999999999)
+
+DZ = 3
+
+xlist = np.linspace(-DZ, DZ, DZ * 10)
+ylist = np.linspace(-DZ, DZ, DZ * 10)
+
+X, Y = np.meshgrid(xlist, ylist)
+Z = np.zeros((len(xlist), len(ylist)))
+for i in range(len(xlist)):
+    for j in range(len(ylist)):
+        Z[-i-1, -j-1] = f(fun, [xlist[i], ylist[j]])
+
+
+fig,ax=plt.subplots(1,1)
+cp = ax.contourf(X, Y, Z)
+
+fig.colorbar(cp)
+ax.scatter(a.best[2][0], a.best[2][1], c = "red")
+plt.show()
